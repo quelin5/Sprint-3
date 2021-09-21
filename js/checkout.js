a27496fed0a5bf8da35605697d80fae8b4ba6596
quelin5 <raquelsospedra51298@gmail.com>
2021-09-21 11:13:31 +0200

    nivel 1

--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,95 @@
+
+//VARIABLES
 // Get the input fields
+var formulario = document.querySelector("form");
+
 var password = document.querySelector(".password");
 var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
+var nombre = document.querySelector('.name');
+var surname = document.getElementById("lastName");
+var email = document.getElementById("email");
+var address = document.getElementById("address");
+
+var formulario = document.querySelector('.form');
 
 // Get the error elements
 var errorPassword = document.getElementById("errorPassword");
 var errorName = document.getElementById('errorName');  
 var errorPhone = document.getElementById('errorPhone');  
 
+//EventListeners
+password.addEventListener('blur', validate);
+phone.addEventListener('blur', validate);
+nombre.addEventListener('blur', validate);
+email.addEventListener('blur', validate);
+surname.addEventListener('blur', validate);
+address.addEventListener('blur', validate);
+
 // Exercise 9
-function validate() {
-    // Validate fields entered by the user: name, phone, password, and email
+function validate(e) {
+
+//Validar email - FUNCIONA
+    const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; //para comprobar que se ha escrito un dominio válido. 
+    
+    if (e.target.type = 'email' && e.target.value > ''){
+        if (er.test(e.target.value)){
+            console.log('email valido');
+        } else {
+            mostrarError('Email no válido.');
+        }
+    }
+
+//Comprobar si el telefono son solo numeros: - FUNCIONA
+    if (e.target.type == "number" && e.target.value.length > 0){
+        if (validarNumero(e.target.value)){
+            console.log('Número válido');
+        } else {
+            console.log('El número de teléfono no es válido.');
+            e.target.style.borderColor = 'red';
+        }
+    }    
+
+//Comprobar que la contraseña contenga números y letras: - FUNCIONA
+    const erNumLetra = /^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$/;
+
+    if (e.target.type="password" && e.target.value > ''){
+        if (erNumLetra.test(e.target.value)){
+            console.log('BIEN Contraseña contiene num y letras.');
+        } else {
+            console.log('MAL Contraseña NO contiene num y letras.');
+            e.target.style.borderColor = 'red';
+        }
+    }
+
+//Validar que contienen +3 caracteres - FUNCIONA
+    if (password.value.length >= 3 && phone.value.length >= 3 && nombre.value.length >= 3 && surname.value.length >= 3 && email.value.length >= 3 && address.value.length >= 3){
+        console.log('BIEN, todos los campos contienen más de tres letras.')
+    } else {
+        mostrarError('Todos los campos deben tener un mínimo de tres caracteres.');
+        //e.target.style.borderColor = 'red';
+    }
+
+//Validar que todos los campos estén: - FUNCIONA
+    if (password.value != '' && phone.value != 0 && nombre.value != '' && surname.value != '' && email.value != '' && address.value != ''){
+        console.log('Están todos los campos');
+    } else {
+        mostrarError('Todos los campos son obligatorios.');
+    }
+}
+
+//Filtrar solo numeros:
+    function validarNumero(value){
+        var str = value.toString().replace(/\s/g, '');
+        return str.length === 9 && /^[679]{1}[0-9]{8}$/.test(str);
+    }
+
+//Mostrar mensaje de error:
+function mostrarError(mensaje){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = mensaje;
+    mensajeError.style.textAlign = "center";
+    mensajeError.style.margin = "50px 200px";
+    mensajeError.style.border = "1px solid red";
+
+    formulario.appendChild(mensajeError);
 }
--- js/grocery.js
+++ js/grocery.js
@@ -1,6 +1,45 @@
+//acceder al archivo json: //los datos con los que trabajamos
+//crear constante fetch
+document.addEventListener('DOMContentLoaded', () => {
+fetchData();
+})
+const fetchData = async () => {
+    try {
+        const res = await fetch('api.json')
+        const data = await res.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 // Exercise 11
+
+//VARIABLES 
+var cartList = []; //carrito de la compra
+var cart = [];
+var subtotal = {
+    grocery: {
+        value: 0,
+        discount: 0
+    },
+    beauty: {
+        value: 0,
+        discount: 0
+    },
+    clothes: {
+        value: 0,
+        discount: 0
+    },
+};
+var total = 0;
+let subtotalGrocery = 0;
+let subtotalBeauty = 0;
+let subtotalClothes = 0;
+
 // Move this variable to a json file and load the data in this js
-var products = [
+//esto es un array de objetos
+var products = [ 
     {
         name: 'cooking oil',
         price: 10.5,
@@ -47,36 +86,20 @@ var products = [
         type: 'clothes'
     }
 ]
-var cartList = [];
-var cart = [];
-var subtotal = {
-    grocery: {
-        value: 0, 
-        discount: 0
-    },
-    beauty: {
-        value: 0, 
-        discount: 0
-    },
-    clothes: {
-        value: 0, 
-        discount: 0
-    },
-};
-var total = 0;
+
+
+//FUNCIONES
 
 // Exercise 1
 function addToCartList(id) {
     // 1. Loop for to the array products to get the item to add to cart.
-    
-    cartList.push(products[id-1]);
-    console.log(cartList);
+
+    cartList.push(products[id - 1]);
+    console.log('El carrito ', cartList);
     calculateSubtotals();
-    console.log(subtotal);
+    console.log('Cálculo del subtotal ', subtotal);
     calculateTotal();
-    console.log(total);
-    applyPromotionsSubtotals();
-    console.log(total)
+    generateCart();   
 }
 
 // Exercise 2. Empty the shopping cart. 
@@ -84,28 +107,25 @@ function cleanCart() {
     cartList.length = 0; //set the array to 0. 
 }
 
-
-
 // Exercise 3
 function calculateSubtotals() {
     // 1. Create a for loop on the "cartList" array 
     // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
-   
-    let subtotalGrocery = 0;
-    let subtotalBeauty = 0;
-    let subtotalClothes = 0;
 
-    for (let i = 0; i < cartList.length; i++){
-        switch(cartList[i].type){
-            case 'grocery': 
+    subtotalGrocery = 0; 
+    subtotalClothes = 0;
+    subtotalBeauty = 0; 
+    for (let i = 0; i < cartList.length; i++) {
+        switch (cartList[i].type) {
+            case 'grocery':
                 //get the price + añadir a subtotalGrocery
                 subtotalGrocery += cartList[i].price;
                 break;
             case 'beauty':
-                subtotalBeauty += cartList[i].price; 
+                subtotalBeauty += cartList[i].price;
                 break;
             case 'clothes':
-                subtotalClothes += cartList[i].price; 
+                subtotalClothes += cartList[i].price;
                 break;
         }
     }
@@ -116,36 +136,37 @@ function calculateSubtotals() {
 
 // Exercise 4
 function calculateTotal() {
+    //applyPromotionsSubtotals(); Si lo descomento aparece error RangeTotal en consola. 
+
     total = 0;
     // Calculate total price of the cart.
     for (let x in subtotal) { //new way of expressing a condition. Looked up on the Internet! 
         total += subtotal[x].value;
+        total -= subtotal[x].discount;
     }
-    console.log(total);
+    console.log('Cálculo del total ', total);
 }
 
 // Exercise 5
 function applyPromotionsSubtotals() {
-let cookingOil = 0;
-let cupcakeMix = 0;
+    let totalCookingOil = 0;
+    let totalCupcakeMix = 0;
+    subtotal.grocery.discount = 0; 
     //método find. encuentra num de cooking oils + cupcake mix
-    for (let i = 0; i < cartList.length; i++){
-        if (cartList[i].name = 'cooking oil'){
-            cookingOil += 1;
-        } else if (cartList[i].name = 'Instant cupcake mixture'){
-            cupcakeMix += 1;
+    for (let i = 0; i < cartList.length; i++) {
+        if (cartList[i].name === 'cooking oil') {
+            totalCookingOil += 1;
+        } else if (cartList[i].name === 'Instant cupcake mixture') {
+            totalCupcakeMix += 1;
         }
     }
 
-    if (cookingOil >= 3) {
-        products[1].price = 10; 
-       //NO subtotalGrocery += cookingOil * products[1].price; //subtotalGrocery es botellas de aceite x el precio de cada una; 
+    if (totalCookingOil >= 3){
+        subtotal.grocery.discount += (totalCookingOil*10.50 - totalCookingOil*10);
+    } 
 
-    }
-
-    if (cupcakeMix >= 10){
-        products[3].price = 2/3 *  products[3].price; //is that right? 
-        //NO subtotalGrocery += cupcakeMix * products[3].price;
+    if (totalCupcakeMix >= 10){
+        subtotal.grocery.discount += ((totalCupcakeMix*5) - (totalCupcakeMix*(5*2/3)));
     }
 
     calculateTotal();
@@ -156,27 +177,89 @@ function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
 
-    for (let i = 0; i < cartList.length; i++){
+    cart = [];
+    for (let i = 0; i < cartList.length; i++) {
+        if(cart.length > 0){ 
+            let existe = false;
+            for(let j = 0; j < cart.length; j++) {
+                if(cartList[i].name === cart[j].name){ //ya existe? 
+                    existe = true;
+                    cart[j].quantity++;
+                    cart[j].subtotal = cart[j].quantity*cart[j].price; 
+                    break; //cuando lo encuentre sale. Se lleva la posición donde lo ha encontrado al siguiente paso. 
+                }
+            }
 
+            if (!existe) {
+                cart.push(cartList[i]);
+                cart[cart.length-1].quantity = 1;
+                cart[cart.length-1].subtotal = cart[cart.length-1].quantity*cart[cart.length-1].price;
+            }
+
+        } else {
+            cart.push(cartList[i]);
+            cart[0].quantity = 1;
+            cart[0].subtotal = cart[0].quantity*cart[0].price;
+        }
     }
+
+    applyPromotionsCart();
+
+    console.log('Cart :', cart);
 }
 
 // Exercise 7
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+
+    for(let i = 0; i < cart.length; i++) {
+        if (cart[i].name === 'cooking oil'){
+            if (cart[i].quantity >= 3){
+                cart[i].subtotalWithDiscount = cart[i].quantity*10;
+            }
+        }
+
+        if(cart[i].name === 'Instant cupcake mixture'){
+            if (cart[i].quantity >= 10){
+                cart[i].subtotalWithDiscount = cart[i].quantity*(5*(2/3));
+            }
+        }
+    }
 }
 
 // Exercise 8
 function addToCart(id) {
-    addToCartList(id); //esta funcion llama a CartList. 
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
+
+    if (cart.length > 0){ 
+        for (let i = 0; i < cart.length; i++){
+            if (products[id-1].name === cart[i].name){
+                cart[i].quantity++;
+                cart[i].subtotal = cart[i].quantity*cart[i].price; 
+            } else { 
+                cart.push(products[id-1]);
+                cart[0].quantity = 1;
+                cart[0].subtotal = cart[0].quantity*cart[0].price;
+            }
+        } 
+    } else {
+        cart.push(products[id-1]);
+        cart[0].quantity = 1;
+        cart[0].subtotal = cart[0].quantity*cart[0].price;
+    }
+
+    console.log(cart);
 }
 
 // Exercise 9
 function removeFromCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
+
+    for (let i = 0; i < products.length; i++){
+        
+    }
 }
 
 
